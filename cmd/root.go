92aafea6365199e3b61e910335abeb5b810bdd97
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-19 14:17:47 +0100

    chore: Add root command (scaffold, no logic)

---
+++ cmd/root.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "netwatch",
+	Short: "Netwatch is a CLI tool for monitoring local network devices",
+	Long: `Netwatch is a self-hosted CLI-based network monitoring tool. It scans your local subnet for connected devices, 
+tracks changes over time, and logs all activity. Netwatch is designed to run continuously or on-demand, and optionally 
+serve a local HTTP status interface.
+
+This tool is useful for monitoring your home lab, detecting new devices on your network, or tracking the uptime of known machines.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.netwatch.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
