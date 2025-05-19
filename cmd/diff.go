7c8b3d85374af55506c18a56100161695fcd9999
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-19 14:20:33 +0100

    chore: add diff subcommand and wired to device command

---
+++ cmd/diff.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// diffCmd represents the diff command
+var diffCmd = &cobra.Command{
+	Use:   "diff",
+	Short: "Compare the two most recent scans and show changes",
+	Long: `Loads the two latest scan results and compares them to detect added or removed devices. 
+Reports any hosts that are new since the last scan or no longer detected.
+
+Example:
+  netwatch devices diff --format text`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("diff called")
+	},
+}
+
+func init() {
+	deviceCmd.AddCommand(diffCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// diffCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// diffCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
