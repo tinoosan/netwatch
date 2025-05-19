d4eb3771a51c37a9ae2b4c314bc9e879c38c9818
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-19 15:35:54 +0100

    refactor: removed global variable for flags

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -23,20 +23,19 @@ You can optionally expose a local HTTP server to view status and history via end
 Example:
   netwatch monitor --subnet 192.168.1.0/24 --interval 300 --port 9000 --logfile ./netwatch.log`,
 	Run: func(cmd *cobra.Command, args []string) {
+		subnet, _ := cmd.Flags().GetString("subnet")
+		interval, _ := cmd.Flags().GetInt("interval")
+		port, _ := cmd.Flags().GetInt("port")
+		logfile, _ := cmd.Flags().GetString("logfile")
+		env, _ := cmd.Flags().GetString("env")
 		fmt.Printf("flags set: subnet %s | interval %d | port %d | logfile %s | env %s\n", subnet, interval, port, logfile, env)
 	},
 }
 
-var (
-	subnet string
-	interval int
-	port int
-	logfile string
-	env string
-)
 
 func init() {
 	rootCmd.AddCommand(monitorCmd)
+
 
 	// Here you will define your flags and configuration settings.
 
@@ -47,10 +46,10 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	//monitorCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	monitorCmd.Flags().StringVarP(&subnet, "subnet", "s", "192.168.1.0/24", "CIDR block of the subnet to scan")
-	monitorCmd.Flags().IntVarP(&interval, "interval", "i", 300, "Scan frequency in seconds")
-	monitorCmd.Flags().IntVarP(&port, "port", "p", 9000, "Port to run the optional HTTP status server (0 disables)")
-	monitorCmd.Flags().StringVarP(&logfile, "logfile", "l", "./netwatch.log", "Path to write logs to")
-	monitorCmd.Flags().StringVarP(&env, "env", "e", "dev", "Environment mode: dev or prod")
+	monitorCmd.Flags().StringP("subnet", "s", "192.168.1.0/24", "CIDR block of the subnet to scan")
+	monitorCmd.Flags().IntP("interval", "i", 300, "Scan frequency in seconds")
+	monitorCmd.Flags().IntP("port", "p", 9000, "Port to run the optional HTTP status server (0 disables)")
+	monitorCmd.Flags().StringP("logfile", "l", "./netwatch.log", "Path to write logs to")
+	monitorCmd.Flags().StringP("env", "e", "dev", "Environment mode: dev or prod")
 
 }
