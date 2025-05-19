43bd0a16cc26f0f857d7007824cab6772db6f296
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-19 15:24:53 +0100

    feat: add monitor command flags (subnet, interval, port, logfile, env)

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -23,9 +23,17 @@ You can optionally expose a local HTTP server to view status and history via end
 Example:
   netwatch monitor --subnet 192.168.1.0/24 --interval 300 --port 9000 --logfile ./netwatch.log`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("monitor called")
+		fmt.Printf("flags set: subnet %s | interval %d | port %d | logfile %s | env %s\n", subnet, interval, port, logfile, env)
 	},
 }
+
+var (
+	subnet string
+	interval int
+	port int
+	logfile string
+	env string
+)
 
 func init() {
 	rootCmd.AddCommand(monitorCmd)
@@ -38,5 +46,11 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// monitorCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//monitorCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	monitorCmd.Flags().StringVarP(&subnet, "subnet", "s", "192.168.1.0/24", "CIDR block of the subnet to scan")
+	monitorCmd.Flags().IntVarP(&interval, "interval", "i", 300, "Scan frequency in seconds")
+	monitorCmd.Flags().IntVarP(&port, "port", "p", 9000, "Port to run the optional HTTP status server (0 disables)")
+	monitorCmd.Flags().StringVarP(&logfile, "logfile", "l", "./netwatch.log", "Path to write logs to")
+	monitorCmd.Flags().StringVarP(&env, "env", "e", "dev", "Environment mode: dev or prod")
+
 }
