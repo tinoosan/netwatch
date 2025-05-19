dd630708368be33fa8a6bfc6c677d81d9bc5a2a6
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-19 14:21:49 +0100

    chore: Add status command scaffold (no logic)

---
+++ cmd/status.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Expose current scan status and history over HTTP",
+	Long: `Exposes a basic HTTP API with endpoints for current status and recent scan history. 
+Intended to be used in conjunction with the monitor command.
+
+Endpoints:
+  /status   - Latest device state
+  /history  - Most recent scan snapshots`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("status called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
