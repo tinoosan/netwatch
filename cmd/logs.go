c8fda3183e476b10d697c57b2e2b663e248346ff
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-19 14:21:26 +0100

    chore: Add logs command scaffold (no logic)

---
+++ cmd/logs.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// logsCmd represents the logs command
+var logsCmd = &cobra.Command{
+	Use:   "logs",
+	Short: "View and filter Netwatch log output",
+	Long: `Reads the Netwatch log file and optionally filters by time or device. You can use this to 
+inspect historical scan results, device changes, or internal system events.
+
+Example:
+  netwatch logs --since 1h --device 192.168.1.42 --limit 50`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("logs called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(logsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// logsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// logsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
