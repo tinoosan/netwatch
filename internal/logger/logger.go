032faa49e3b2bade60e01e2e56bcf0aa79969440
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2025-05-21 20:04:30 +0100

    test(logger): verify file output and buffer reset in Logger.Log

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -9,20 +9,20 @@ import (
 
 type Logger struct {
 	logger   *log.Logger
-	buf      bytes.Buffer
+	Buffer      bytes.Buffer
 	File     *os.File
 	prefix   string
 	filename string
 }
 
 func New(filename string, prefix string) *Logger {
-	var buf bytes.Buffer
+	var Buffer bytes.Buffer
 	f, err := os.OpenFile(filename+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("log file could not be created: %v", err)
 	}
 	return &Logger{
-		buf:      buf,
+		Buffer:      Buffer,
 		File:     f,
 		prefix:   prefix + ": ",
 		filename: filename,
@@ -31,13 +31,13 @@ func New(filename string, prefix string) *Logger {
 
 func (l *Logger) Log(message string) error {
 	if l.logger == nil {
-		l.logger = log.New(&l.buf, l.prefix, log.Default().Flags())
+		l.logger = log.New(&l.Buffer, l.prefix, log.Default().Flags())
 	}
 	l.logger.Print(message)
-	_, err := l.File.Write(l.buf.Bytes())
+	_, err := l.File.Write(l.Buffer.Bytes())
 	if err != nil {
 		return fmt.Errorf("could not print to log file %v: ", err)
 	}
-	l.buf.Reset()
+	l.Buffer.Reset()
 	return nil
 }
--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -1,6 +1,7 @@
 package logger_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/tinoosan/netwatch/internal/logger"
@@ -8,11 +9,32 @@ import (
 
 func TestLog(t *testing.T) {
 	t.Run("test", func(t *testing.T) {
+
+		file := "./logging_test.log"
+
 		logger := logger.New("logging_test", "test")
 		err := logger.Log("This is a test message")
+		if err != nil {
+			t.Errorf("expected nil got %v", err)
+		}
+		dat, err := os.ReadFile(file)
+
+		defer logger.File.Close()
+		if len(dat) == 0 {
+			t.Error("no contents in file")
+		}
+
+		if len(logger.Buffer.Bytes()) != 0 {
+			t.Errorf("buffer not cleared. expected 0 got %d", len(logger.Buffer.Bytes()))
+		}
+
 		if err != nil {
 			t.Errorf("want nil got %s", err)
 		}
 
+		err = os.Remove(file)
+		if err != nil {
+			t.Error("could not remove file")
+		}
 	})
 }
